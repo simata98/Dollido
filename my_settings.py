995dba81bd0decb7d289995ccd6ec7abde91f050
simata <wlsrnr905@naver.com>
2022-12-05 14:12:20 +0900

    데이터베이스 연동 및 JWT 로그인 구현

---
+++ my_settings.py
@@ -0,0 +1,17 @@
+# MySQL 데이터베이스 설정
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+DATABASES = {
+  'default' : {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'DOLLIDO',
+    'USER': 'root',
+    'PASSWORD': 'wlsrnr905!',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
+}
+
+SECRET_KEY = 'django-insecure-=&m=-bhcn&232@%588=ig8%()b@-0ti(a8g$)z#wyz8^(-6ll%'
