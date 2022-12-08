f544e1fc0278803c8e63c0f1e2653141b5e02d2b
soon9 <81350489+hsk980911@users.noreply.github.com>
2022-12-08 17:14:28 +0900

    models.py 수정 및 mysql 연동 중

--- post/models.py
+++ post/models.py
@@ -10,25 +10,22 @@ class Category(models.Model):
 
 class ApiListId(models.Model):
   # API list primary key
-  api_lst_id = models.AutoField(primary_key=True)
+  atcId = models.AutoField(primary_key=True)
   # 물품명
-  lstPrdtNm = models.CharField(max_length=200, null=True, default = '')
+  fdPrdtNm = models.CharField(max_length=200, null=True, default = '')
   # 분실물 이미지 명
-  lstFilePathImg = models.CharField(max_length=300)
-  # 분실물 상태명
-  lstSteNm = models.CharField(max_length=100)
-  # 분실지역명
-  lstLctNm = models.CharField(max_length=200)
-  # 분실장소
-  lstPlace = models.CharField(max_length=100)
-  # 기관전화번호
-  tel = models.CharField(max_length=15)
-  # 기관명
-  orgNm = models.CharField(max_length=100)
-  # 분실일자
-  lstYmd = models.DateField(max_length=10, auto_now=False, auto_now_add=False)
+  fdFilePathImg = models.CharField(max_length=300)
+  # 게시제목
+  fdSbjt = models.CharField(max_length=100)
+  # 보관 장소
+  depPlace = models.CharField(max_length=30)
+  # 습득일자
+  
+  fdYmd = models.DateField(max_length=10, auto_now=False, auto_now_add=False)
   # 카테고리 (외래키)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  # 색상명
+  clrNm = models.CharField(max_length=10)
   
   
   
