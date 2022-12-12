4a52444a986f71948bc8d6a1ac255b8b24a2c084
byungsub <nq8947@naver.com>
2022-12-12 12:01:50 +0900

    경찰청 데이터 db 구현

--- dollido/urls.py
+++ dollido/urls.py
@@ -20,7 +20,7 @@ from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/token/', obtain_jwt_token),
-    path('accounts/token/verify/', verify_jwt_token),
-    path('accounts/token/refresh/', refresh_jwt_token),
+    # path('accounts/token/', obtain_jwt_token),
+    # path('accounts/token/verify/', verify_jwt_token),
+    # path('accounts/token/refresh/', refresh_jwt_token),
 ]
