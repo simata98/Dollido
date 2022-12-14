efe5f373beb04c9b92e18143482bd8bee6f31681
hyeonju0121 <a01085249773@gmail.com>
2022-12-14 14:30:08 +0900

    게시판 돌리도 db 적용 완료
    Former-commit-id: 1975ae3e34c103762956fded6b425e074a7089dc

--- mysite/dollido/forms.py
+++ mysite/dollido/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from .models import Lost
-#import datetime
+from .models import DollidoLstId
+import datetime
 
 YEARS= [x for x in range(2022,2023)]
 
-
+'''
 class LostForm(forms.ModelForm):
     item = forms.CharField(label='분실물 이름',widget= forms.TextInput(attrs={'class':'form-control',' placeholder':'분실물 이름을 입력하세요.'}))
     description = forms.CharField(label='특이사항',widget= forms.TextInput(attrs={'class':'form-control',' placeholder':'특이사항을 입력하세요.'}))
@@ -13,4 +13,17 @@ class LostForm(forms.ModelForm):
 
     class Meta:
         model = Lost
-        fields = ['item', 'description', 'image']
+        fields = ['item', 'description', 'image']'''
+
+class LostForm(forms.ModelForm):
+    lstPrdtNm = forms.CharField(label='습득물 이름',widget= forms.TextInput(attrs={'class':'form-control',' placeholder':'분실물 이름을 입력하세요.'}))
+    lstcontent = forms.CharField(label='상세설명',widget= forms.TextInput(attrs={'class':'form-control',' placeholder':'특이사항을 입력하세요.'}))
+    lstFilePathImg = forms.ImageField(label='사진')
+    lstYmd = forms.DateField(label='습득일자', initial=datetime.date.today, widget=forms.SelectDateWidget(years=YEARS))
+    
+    selectoption = [('kt탄방타워 정문','kt탄방타워 정문')]
+    lstPlace = forms.ChoiceField(label='보관장소', widget= forms.Select(attrs={ "class":"form-control input_felid"}),choices=selectoption)
+    
+    class Meta:
+        model = DollidoLstId
+        fields = ['lstPrdtNm', 'lstcontent', 'lstFilePathImg', 'lstYmd', 'lstPlace']
--- mysite/dollido/models.py
+++ mysite/dollido/models.py
@@ -13,7 +13,24 @@ class Lost(models.Model):
         return self.item
     class Meta:
         db_table = 'lost_item'
+
+class DollidoLstId(models.Model):
+    # 물품명
+    lstPrdtNm = models.CharField('습득물 이름',max_length=200)
+    # 습득물 이미지
+    lstFilePathImg = models.ImageField('사진 등록', upload_to='lost_image', blank=True)
+    # 습득물 상세설명
+    lstcontent = models.CharField('특이사항',max_length=500, default='')
+    # 습득일자
+    lstYmd = models.DateField(max_length=10, auto_now=False, auto_now_add=False)
+    # 보관장소 (수거함)
+    lstPlace = models.CharField(blank = True, max_length =50)
+    # 게시일자
+    create_date = models.DateTimeField(default=timezone.now)
   
+    def __str__(self):
+            return self.item
+
 
 class ApiListId(models.Model):
   # API list primary key
