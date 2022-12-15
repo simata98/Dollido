8cab182be82f312ba078695b18bc504b8f48f557
byungsub <nq8947@naver.com>
2022-12-15 14:36:01 +0900

    돌리도 게시판 직찍 기능 구현중
    Former-commit-id: 7e9a26c420e8e9098bacd3b5773bf55dca11a52a

--- mysite/dollido/admin.py
+++ mysite/dollido/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Lost
+from . models import Lost, DollidoLstId
 
 # Register your models here.
 admin.site.register(Lost)
+admin.site.register(DollidoLstId)
--- mysite/dollido/models.py
+++ mysite/dollido/models.py
@@ -30,23 +30,3 @@ class DollidoLstId(models.Model):
   
     def __str__(self):
             return self.item
-
-
-class ApiListId(models.Model):
-  # API list primary key
-  atcId = models.CharField(max_length=30, primary_key=True)
-  # 물품명
-  fdPrdtNm = models.CharField(max_length=500, null=True, default = '')
-  # 분실물 이미지 명
-  fdFilePathImg = models.CharField(max_length=300)
-  # 게시제목
-  fdSbjt = models.CharField(max_length=500)
-  # 보관 장소
-  depPlace = models.CharField(max_length=30)
-  # 습득일자  
-  fdYmd = models.DateField(max_length=10, null = True)
-  # 카테고리 (외래키)
-  # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-  category = models.CharField(max_length=20)
-  # 색상명
-  clrNm = models.CharField(max_length=10)
