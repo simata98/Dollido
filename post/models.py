32cb0815de442bbef1a0163480fe743a29c15488
choiseeyou <110075391+choiseeyou@users.noreply.github.com>
2022-12-21 21:17:11 +0900

    정형님+희건님 코드 병합중
    Former-commit-id: cfa6e47c747a69dad21f8c71d3b4d45f4d866d9b

--- post/models.py
+++ post/models.py
@@ -29,20 +29,24 @@ class ApiListId(models.Model):
   clrNm = models.CharField(max_length=10)
 
 class DollidoLstId(models.Model):
+    class Meta:
+        verbose_name_plural = '돌리도 게시판'
     # 사용자 (외래키)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='작성자', null=True, blank=True, related_name='user_id')
     # 물품명
-    lstPrdtNm = models.CharField('습득물 이름',max_length=200)
+    lstPrdtNm = models.CharField('습득물 이름',max_length=200, blank=True)
     # 습득물 이미지
     lstFilePathImg = models.ImageField('사진 등록', upload_to='public', blank=True)
     # 습득물 상세설명
     lstcontent = models.CharField('특이사항',max_length=500, default='')
     # 습득일자
-    lstYmd = models.DateField(max_length=10, auto_now=False, auto_now_add=False)
+    lstYmd = models.DateField(max_length=10, auto_now=False, auto_now_add=False, blank=True)
     # 보관장소 (수거함)
     lstPlace = models.CharField(blank = True, max_length =50)
     # 게시일자
     create_date = models.DateTimeField(default=timezone.now)
+    # 물건을 찾았는지
+    find_status = models.BooleanField(default=False)
 
     def __str__(self):
-            return self.item
+        return self.lstPrdtNm
