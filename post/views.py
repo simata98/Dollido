6a2611bbc75b714958cea938862e25d0a12733ed
simata98 <wlsrnr905@naver.com>
2022-12-24 16:18:23 +0900

    색깔 파이프라인 구현중
    Former-commit-id: 62e5838827b7942d82cd63da14b13591050fa64c [formerly f05ba59d0ed13973343928ed84d4e3c46ac94d38]
    Former-commit-id: 2e9eb9b16880ff04f539cf0bd6e303798d56c0f3

--- post/views.py
+++ post/views.py
@@ -9,6 +9,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import PostSerializer
 from accounts.serializers import UserSerializer
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from keras.models import model_from_json
 
 @api_view(['GET', 'POST'])
 def PostList(request):
@@ -22,8 +28,39 @@ def PostList(request):
   elif request.method == 'POST':
     serializer = PostSerializer(data=request.data)
     if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=201)
+      post_instance = serializer.save()
+      json_file = open("post/color_classification/model.json", "r")
+      loaded_model_json = json_file.read()
+      json_file.close()
+      loaded_model = model_from_json(loaded_model_json)
+
+      loaded_model.load_weights('post/color_classification/fashion-EFN-model.best.h5')
+      IMAGE_PATH = serializer.data['lstFilePathImg'][1:]
+      IMAGE_SIZE = 224
+      THRESHOLD = 0.1
+      
+      def predict_color(img_path=IMAGE_PATH, isize=IMAGE_SIZE, thrs=THRESHOLD):
+        classes = ['Beige', 'Black', 'Blue', 'Brown', 'Gold', 'Green', 'Grey', 'Maroon', 'Navy',
+          'Olive', 'Orange', 'Pink', 'Purple', 'Red', 'Silver', 'White', 'Yellow']
+      
+        image = keras.preprocessing.image.load_img(img_path, target_size=(isize, isize))
+        data = np.asarray(image)
+        ndata = np.expand_dims(data, axis=0)
+        y_prob = loaded_model.predict(ndata/255)
+        y_prob.argmax(axis=-1)
+        
+        color_result_list = [classes[i] for i in np.where(np.ravel(y_prob)>thrs)[0]]
+        confident_result_list = [np.ravel(y_prob)[i] for i in list(np.where(np.ravel(y_prob)>thrs)[0])]
+        classification_result = color_result_list[confident_result_list.index(max(confident_result_list))]
+        
+        print('color', color_result_list)
+        print('values', confident_result_list)
+        print('results', classification_result)
+        return classification_result
+      
+      newdict={'clrNm':predict_color(img_path=IMAGE_PATH)}
+      newdict.update(serializer.data)
+      return Response(newdict, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=404)
 
 @api_view(['GET', 'PUT', 'DELETE'])
