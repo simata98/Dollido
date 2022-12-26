5a878c35ffa01346bdb51ad0738fd66052f8eec3
choiseeyou <110075391+choiseeyou@users.noreply.github.com>
2022-12-26 17:48:37 +0900

    정형님꺼랑 병합 직전
    Former-commit-id: 2cc47c238a66ee0c3c550dfdb1a7ec47e2341579

--- dollido/settings.py
+++ dollido/settings.py
@@ -142,7 +142,6 @@ LANGUAGE_CODE = 'ko-kr'
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
-
 USE_TZ = False
 
 
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -4,8 +4,26 @@ import { Link } from "react-router-dom";
 // import {jwtUtils} from "../utils/jwtUtils";
 import { useEffect, useState } from "react";
 // import {setToken} from "../redux/reducers/AuthReducer";
+import { Mobile, Pc } from '../pages/responsive';
+import Popup from "reactjs-popup";
+import React from "react";
+import NoAuthMenubar from "./noAuthMenu"
+import AuthMenubar from "./isAuthMenu"
+import BurgerIcon from "./burgerIcon"
+import "./index.css"
 
-const Header = () => {
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  marginTop: "40px"
+};
+const contentStyle = {
+  background: "rgba(255,255,255,0)",
+  width: "80%",
+  border: "none"
+};
+
+const Header = ({children, open, ...props}) => {
   // const dispatch = useDispatch();
   // const token = useSelector(state => state.Auth.token);
   const token = localStorage.getItem("token");
@@ -28,36 +46,75 @@ const Header = () => {
     }
   }
   return (
-    <div className="header-wrapper">
-      <div className="header-title">
-        <Link to="/">
-          <img
-            src = {`/title.png`}
-            width = '50%'
-            alt = 'title'
-          />
-        </Link>
-      </div>
-      <div className="header-menu">
-        {/* <Link to="/mydatagrid">게시판</Link>
-        <Link to="/about">내 게시물</Link> */}
-        {isAuth ? (
-          <>
-            <Link to="/mydatagrid">Lost112</Link>
-            <Link to="/dollidolist">Dollido</Link>
-            <Link to="/about">내 게시물</Link>
-            <Link to="#" onClick={onLogout}>로그아웃</Link>
-          </>
-        ) : (
-          <>
-            <Link to="/signin">로그인</Link>
-            <Link to="/agreement">회원가입</Link>
-          </>
-        )}
+    <>
+    <Pc>
+      <div className="header-wrapper">
+        <div className="header-title">
+          <Link to="/">
+            <img
+              src = {`/title.png`}
+              width = '50%'
+              alt = 'title'
+            />
+          </Link>
+        </div>
+        <div className="header-menu">
+          {/* <Link to="/mydatagrid">게시판</Link>
+          <Link to="/about">내 게시물</Link> */}
+          {isAuth ? (
+            <>
+              <Link to="/mydatagrid">Lost112</Link>
+              <Link to="/dollidolist">Dollido</Link>
+              <Link to="/about">내 게시물</Link>
+              <Link to="#" onClick={onLogout}>로그아웃</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/signin">로그인</Link>
+              <Link to="/agreement">회원가입</Link>
+            </>
+          )}
 
-        <Link to="/about">about</Link>
-      </div>
-    </div>
+          <Link to="/about">about</Link>
+        </div>
+        </div>
+      </Pc>
+      
+      {/* https://codesandbox.io/s/k2x7l5jy27?file=/src/Menu.js */}
+      <Mobile>
+      {isAuth ? (
+            <>
+              <div style={styles}>
+                <Popup
+                  modal
+                  overlayStyle={{ background: "rgba(255,255,255,0.98)" }}
+                  contentStyle={contentStyle}
+                  closeOnDocumentClick={false}
+                  trigger={open => <BurgerIcon open={open} />}
+                >
+                  {close => <AuthMenubar close={close} />}
+              </Popup>
+            </div>
+            
+            </>
+          ) : (
+            <>
+              <div style={styles}>
+                <Popup
+                  modal
+                  overlayStyle={{ background: "rgba(255,255,255,0.98)" }}
+                  contentStyle={contentStyle}
+                  closeOnDocumentClick={false}
+                  trigger={open => <BurgerIcon open={open} />}
+                >
+                  {close => <NoAuthMenubar close={close} />}
+                </Popup>
+              </div>
+            </>
+          )}
+
+      </Mobile>
+    </>
   );
 };
 
