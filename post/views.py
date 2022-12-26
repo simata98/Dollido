402356f1457404efd9abf5313c0c886d1b793166
simata98 <wlsrnr905@naver.com>
2022-12-26 12:27:39 +0900

    메타데이터 기능 추가
    Former-commit-id: cf182ffdba480ba1bc7c5cda4885e0566a5fa061 [formerly 79a73c636a1e4ab4fe9b881cd330f4eb0a8a2aa3]
    Former-commit-id: 99e48646886757893d7fc15c14c28cd4112a8974

--- post/views.py
+++ post/views.py
@@ -19,6 +19,9 @@ from keras.models import model_from_json
 from ast import literal_eval
 import collections
 import yolov5
+# 이미지 메타데이터 관련
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 @api_view(['GET', 'POST'])
 def PostList(request):
@@ -67,8 +70,8 @@ def PostList(request):
       # !yolo 파이프라인
       def predict_yolo(img_path=IMAGE_PATH, isize=IMAGE_SIZE):
         model = yolov5.load('post/yolo_classification/best.pt')
-        model.conf = 0.25
-        model.iou = 0.45
+        model.conf = 0.15
+        model.iou = 0.25
         
         img = keras.preprocessing.image.load_img(img_path, target_size=(isize, isize))
         results = model(img, size=IMAGE_SIZE)
@@ -76,18 +79,65 @@ def PostList(request):
         results_list = literal_eval(results_list)
         classes_list = [item["name"] for item in results_list]
         results_counter = collections.Counter(classes_list)
+        print(classes_list)
         return classes_list
       
-      # # !이미지 메타데이터 파이프라인
-      # def metadata(img_path=IMAGE_PATH):
+      # !이미지 메타데이터 파이프라인
+      # https://www.jbmpa.com/python_advanced/3
+      def metadata(img_path=IMAGE_PATH):
+        img = Image.open(img_path)
+        info = img._getexif();
+        img.close()
+        
+        taglabel = {}
+        for tag, value in info.items():
+          decoded = TAGS.get(tag, tag)
+          taglabel[decoded] = value
+          
+        print(taglabel['DateTimeOriginal'])
+        print(taglabel['GPSInfo'])
+        
+        exifGPS = taglabel['GPSInfo']
+        latData = exifGPS[2]
+        lonData = exifGPS[4]
 
+        # 도, 분, 초 계산
+        latDeg = latData[0]
+        latMin = latData[1]
+        latSec = latData[2]
+        lonDeg = lonData[0]
+        lonMin = lonData[1]
+        lonSec = lonData[2]
+        
+        # 도, 분, 초로 나타내기
+        Lat = str(int(latDeg)) + "°" + str(int(latMin)) + "'" + str(latSec) + "\"" + exifGPS[1]
+        Lon = str(int(lonDeg)) + "°" + str(int(lonMin)) + "'" + str(lonSec) + "\"" + exifGPS[3]
+        
+        # 위도 계산
+        Lat = (latDeg + (latMin + latSec / 60.0) / 60.0)
+
+        # 북위, 남위인지를 판단, 남위일 경우 -로 변경
+        if exifGPS[1] == 'S': Lat = Lat * -1
+
+        # 경도 계산
+        Lon = (lonDeg + (lonMin + lonSec / 60.0) / 60.0)
+
+        # 동경, 서경인지를 판단, 서경일 경우 -로 변경
+        if exifGPS[3] == 'W': Lon = Lon * -1
+        print(Lat, ",",  Lon)
+        
+        return "https://www.google.com/maps/place/"+str(Lat)+"+"+str(Lon)
       
       new_serializer_data = serializer.data
       predicted_color = predict_color(img_path=IMAGE_PATH)
       new_serializer_data['clrNm'] = predicted_color
       predicted_yolo = predict_yolo(img_path=IMAGE_PATH)
-      product = predicted_color + ' color ' + predicted_yolo[1] + ' ' + predicted_yolo[0]
+      predicted_yolo_str = ' '.join(set(predicted_yolo))
+      product = predicted_color + ' color ' + predicted_yolo_str
       new_serializer_data['lstPrdtNm'] = product
+      img_metadata = metadata(img_path=IMAGE_PATH)
+      new_serializer_data['lstPlace'] = img_metadata
+      
       return Response(new_serializer_data, status=status.HTTP_201_CREATED)
     return Response(new_serializer_data.errors, status=404)
 
