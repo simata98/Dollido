307eba4184f1cf53890ec2bfc28d75249ef2c534
choiseeyou <110075391+choiseeyou@users.noreply.github.com>
2022-12-29 21:48:48 +0900

    Dollidolist 글쓰기버튼구현(+버튼)=>AddDollido로이동
    Former-commit-id: b24c350c46fbc6430b73990fe903fae9901521ec [formerly e103fcfc25d57899bd199d0898ceb9ee77632e5a]
    Former-commit-id: b20a95a491e8db5efeefe743d7f34f7d1eef5b74

--- front/src/pages/Dollidodetail.js
+++ front/src/pages/Dollidodetail.js
@@ -9,31 +9,52 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Box from '@mui/material/Box';
-
-
+import Fab from '@mui/material/Fab';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+import { Button } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 export default function Dollidodetail() {
   const [tasks, setTasks] = useState([]);
+  const [status, setStatus] = useState(false);
   var dollidoimage = tasks.lstFilePathImg
-  var dollidoimage2 = 'images/' + (dollidoimage||'').split("/").pop();
-//   var workList= (data||'').split('.');
+  var dollidoimage2 = 'images/' + (dollidoimage || '').split("/").pop();
+  //   var workList= (data||'').split('.');
   useEffect(() => {
     const code2 = localStorage.getItem("code2");
-
+    console.log(code2)
+    console.log(status)
     axios
       // lost112의 listitem를 받을려고 axios.get(url주소)로 요청함
-      .get(`http://127.0.0.1:8000/post/${code2}`)
+      .get(`http://127.0.0.1:8000/post/${code2}/`)
       .then(response => {
         setTasks(response.data);
-        console.log(code2)
+        setStatus(response.data.find_status)
+        // console.log(code2)
+        // console.log(status)
       });
   }, []);
-  console.log(tasks)
-  console.log(tasks.lstFilePathImg)
-  console.log(tasks.lstFilePathImg)
-//   const dollidoimage = tasks.lstFilePathImg.split("/").pop();
+  // console.log(tasks.id)
+  //   const dollidoimage = tasks.lstFilePathImg.split("/").pop();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const user = {
+      find_status: true,
+    };
+    // console.log(tasks.id)
+    axios
+      .put(`http://localhost:8000/post/${tasks.id}/`, user)
+      .then(response => {
+        console.log(response.data);
+        setStatus(response.find_status)
+        console.log(response.find_status);
+        // form 초기화
+        alert('회수요청');
+        window.location.reload();
+      });
+  };
   return (
-    
+
     <Box
       sx={{
         marginTop: 15,
@@ -68,58 +89,35 @@ export default function Dollidodetail() {
           alt="Paella dish"
         />
         <CardContent>
-          <Typography variant="body2">
-            {'보관장소 : ' + tasks.lstPlace}
-            <br />
-            <br />
+          <Typography mb={2} variant="body2">
             {tasks.lstcontent}
+            <br />
           </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={10}>
+              <Button target='_blank' variant="contained" href={tasks.lstPlace}>
+                위치 보기
+              </Button>
+            </Grid>
+            <Grid item xs={1}>
+              <Button href='/editdollido' variant="contained">
+                수정
+              </Button>
+            </Grid>
+            <Grid item xs={1}>
+              <Button variant="contained">
+                삭제
+              </Button>
+            </Grid>
+          </Grid>
         </CardContent>
-        {/* <CardActions disableSpacing>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <IconButton
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </IconButton>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-              aside for 10 minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-              medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-              occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-              large plate and set aside, leaving chicken and chorizo in the pan. Add
-              pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-              stirring often until thickened and fragrant, about 10 minutes. Add
-              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and
-              peppers, and cook without stirring, until most of the liquid is absorbed,
-              15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-              mussels, tucking them down into the rice, and cook again without
-              stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don&apos;t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
-          </CardContent>
-        </Collapse> */}
       </Card>
-    </Box>
+      <Box component="form" onSubmit={handleSubmit}
+        sx={{ position: 'fixed', left: 20, bottom: 35, display: 'flex', flexDirection: 'column' }}>
+        <Fab disabled={status} type="submit" size='medium' color="primary" aria-label="add">
+          <AutorenewIcon />
+        </Fab>
+      </Box>
+    </Box >
   );
 }
--- front/src/pages/Dollidolist.js
+++ front/src/pages/Dollidolist.js
@@ -6,177 +6,194 @@ import Box from '@mui/material/Box';
 import { useMediaQuery } from "react-responsive";
 // import imgA from 'C:/bigproject/dollido/media/lost_image/halloween-g8058b6ce3_640.png';
 // DataGrid의 컬럼을 하나하나씩 만들어준다
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
 
 const columns = [
-    {
-        field: 'id',
-        headerName: 'ID',
-        width: 100,
-    },
-    {
-        field: 'lstPrdtNm',
-        headerName: 'Product Name',
-        width: 200,
-    },
-    {
-        field: 'lstFilePathImg',
-        headerName: 'Image',
-        width: 200,
-        // headerAlign: 'center',
-        align: 'center',
-        // renderCell를 통해서 <img 이미지링크>를 받아준다
-      renderCell: (params) =>
-        < img src = { 'images/' + params.value.split('/').pop() } height = "100" alt = 'lstFilePathImg' />
-        // <img src={params.value.split("/").pop()} height="100" alt = 'lstFilePathImg'/>
-        // renderCell: (params) => <img src='/halloween-g8058b6ce3_640.png' height="100" alt = 'lstFilePathImg'/>
-    },
-    {
-        field: 'lstcontent',
-        headerName: 'Subject',
-        width: 300,
-    },
-    {
-        field: 'lstPlace',
-        headerName: 'Deposit Place',
-        width: 200,
-    },
-    {
-        field: 'lstYmd',
-        headerName: 'Date',
-        width: 150,
-    },
-    // {
-    //     field: 'category',
-    //     headerName: 'Category',
-    //     width: 200,
-    // },
-    {
-        field: 'create_date',
-        headerName: 'creat_date',
-        width: 150,
-    },
+  {
+    field: 'id',
+    headerName: 'ID',
+    width: 100,
+  },
+  {
+    field: 'lstPrdtNm',
+    headerName: 'Product Name',
+    width: 200,
+  },
+  {
+    field: 'lstFilePathImg',
+    headerName: 'Image',
+    width: 200,
+    // headerAlign: 'center',
+    align: 'center',
+    // renderCell를 통해서 <img 이미지링크>를 받아준다
+    renderCell: (params) =>
+      < img src={'images/' + params.value.split('/').pop()} height="100" alt='lstFilePathImg' />
+    // <img src={params.value.split("/").pop()} height="100" alt = 'lstFilePathImg'/>
+    // renderCell: (params) => <img src='/halloween-g8058b6ce3_640.png' height="100" alt = 'lstFilePathImg'/>
+  },
+  {
+    field: 'lstcontent',
+    headerName: 'Subject',
+    width: 300,
+  },
+  {
+    field: 'lstPlace',
+    headerName: 'Deposit Place',
+    width: 200,
+  },
+  {
+    field: 'lstYmd',
+    headerName: 'Date',
+    width: 150,
+  },
+  // {
+  //     field: 'category',
+  //     headerName: 'Category',
+  //     width: 200,
+  // },
+  {
+    field: 'create_date',
+    headerName: 'creat_date',
+    width: 150,
+  },
+  {
+    field: 'find_status',
+    headerName: 'find_status',
+    width: 150,
+  },
 ];
 
 // ID값의 유니크(key)값을 못찾아서 atcID를 ID key값으로 지정해주는 코드
 const getRowId = row => row.id;
 
 const StyledGridOverlay = styled('div')(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    '& .ant-empty-img-1': {
-      fill: theme.palette.mode === 'light' ? '#aeb8c2' : '#262626',
-    },
-    '& .ant-empty-img-2': {
-      fill: theme.palette.mode === 'light' ? '#f5f5f7' : '#595959',
-    },
-    '& .ant-empty-img-3': {
-      fill: theme.palette.mode === 'light' ? '#dce0e6' : '#434343',
-    },
-    '& .ant-empty-img-4': {
-      fill: theme.palette.mode === 'light' ? '#fff' : '#1c1c1c',
-    },
-    '& .ant-empty-img-5': {
-      fillOpacity: theme.palette.mode === 'light' ? '0.8' : '0.08',
-      fill: theme.palette.mode === 'light' ? '#f5f5f5' : '#fff',
-    },
-  }));
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100%',
+  '& .ant-empty-img-1': {
+    fill: theme.palette.mode === 'light' ? '#aeb8c2' : '#262626',
+  },
+  '& .ant-empty-img-2': {
+    fill: theme.palette.mode === 'light' ? '#f5f5f7' : '#595959',
+  },
+  '& .ant-empty-img-3': {
+    fill: theme.palette.mode === 'light' ? '#dce0e6' : '#434343',
+  },
+  '& .ant-empty-img-4': {
+    fill: theme.palette.mode === 'light' ? '#fff' : '#1c1c1c',
+  },
+  '& .ant-empty-img-5': {
+    fillOpacity: theme.palette.mode === 'light' ? '0.8' : '0.08',
+    fill: theme.palette.mode === 'light' ? '#f5f5f5' : '#fff',
+  },
+}));
 
 function CustomNoRowsOverlay() {
-    return (
-      <StyledGridOverlay>
-        <svg
-          width="120"
-          height="100"
-          viewBox="0 0 184 152"
-          aria-hidden
-          focusable="false"
-        >
-          <g fill="none" fillRule="evenodd">
-            <g transform="translate(24 31.67)">
-              <ellipse
-                className="ant-empty-img-5"
-                cx="67.797"
-                cy="106.89"
-                rx="67.797"
-                ry="12.668"
-              />
-              <path
-                className="ant-empty-img-1"
-                d="M122.034 69.674L98.109 40.229c-1.148-1.386-2.826-2.225-4.593-2.225h-51.44c-1.766 0-3.444.839-4.592 2.225L13.56 69.674v15.383h108.475V69.674z"
-              />
-              <path
-                className="ant-empty-img-2"
-                d="M33.83 0h67.933a4 4 0 0 1 4 4v93.344a4 4 0 0 1-4 4H33.83a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4z"
-              />
-              <path
-                className="ant-empty-img-3"
-                d="M42.678 9.953h50.237a2 2 0 0 1 2 2V36.91a2 2 0 0 1-2 2H42.678a2 2 0 0 1-2-2V11.953a2 2 0 0 1 2-2zM42.94 49.767h49.713a2.262 2.262 0 1 1 0 4.524H42.94a2.262 2.262 0 0 1 0-4.524zM42.94 61.53h49.713a2.262 2.262 0 1 1 0 4.525H42.94a2.262 2.262 0 0 1 0-4.525zM121.813 105.032c-.775 3.071-3.497 5.36-6.735 5.36H20.515c-3.238 0-5.96-2.29-6.734-5.36a7.309 7.309 0 0 1-.222-1.79V69.675h26.318c2.907 0 5.25 2.448 5.25 5.42v.04c0 2.971 2.37 5.37 5.277 5.37h34.785c2.907 0 5.277-2.421 5.277-5.393V75.1c0-2.972 2.343-5.426 5.25-5.426h26.318v33.569c0 .617-.077 1.216-.221 1.789z"
-              />
-            </g>
+  return (
+    <StyledGridOverlay>
+      <svg
+        width="120"
+        height="100"
+        viewBox="0 0 184 152"
+        aria-hidden
+        focusable="false"
+      >
+        <g fill="none" fillRule="evenodd">
+          <g transform="translate(24 31.67)">
+            <ellipse
+              className="ant-empty-img-5"
+              cx="67.797"
+              cy="106.89"
+              rx="67.797"
+              ry="12.668"
+            />
+            <path
+              className="ant-empty-img-1"
+              d="M122.034 69.674L98.109 40.229c-1.148-1.386-2.826-2.225-4.593-2.225h-51.44c-1.766 0-3.444.839-4.592 2.225L13.56 69.674v15.383h108.475V69.674z"
+            />
+            <path
+              className="ant-empty-img-2"
+              d="M33.83 0h67.933a4 4 0 0 1 4 4v93.344a4 4 0 0 1-4 4H33.83a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4z"
+            />
             <path
               className="ant-empty-img-3"
-              d="M149.121 33.292l-6.83 2.65a1 1 0 0 1-1.317-1.23l1.937-6.207c-2.589-2.944-4.109-6.534-4.109-10.408C138.802 8.102 148.92 0 161.402 0 173.881 0 184 8.102 184 18.097c0 9.995-10.118 18.097-22.599 18.097-4.528 0-8.744-1.066-12.28-2.902z"
+              d="M42.678 9.953h50.237a2 2 0 0 1 2 2V36.91a2 2 0 0 1-2 2H42.678a2 2 0 0 1-2-2V11.953a2 2 0 0 1 2-2zM42.94 49.767h49.713a2.262 2.262 0 1 1 0 4.524H42.94a2.262 2.262 0 0 1 0-4.524zM42.94 61.53h49.713a2.262 2.262 0 1 1 0 4.525H42.94a2.262 2.262 0 0 1 0-4.525zM121.813 105.032c-.775 3.071-3.497 5.36-6.735 5.36H20.515c-3.238 0-5.96-2.29-6.734-5.36a7.309 7.309 0 0 1-.222-1.79V69.675h26.318c2.907 0 5.25 2.448 5.25 5.42v.04c0 2.971 2.37 5.37 5.277 5.37h34.785c2.907 0 5.277-2.421 5.277-5.393V75.1c0-2.972 2.343-5.426 5.25-5.426h26.318v33.569c0 .617-.077 1.216-.221 1.789z"
             />
-            <g className="ant-empty-img-4" transform="translate(149.65 15.383)">
-              <ellipse cx="20.654" cy="3.167" rx="2.849" ry="2.815" />
-              <path d="M5.698 5.63H0L2.898.704zM9.259.704h4.985V5.63H9.259z" />
-            </g>
           </g>
-        </svg>
-        <Box sx={{ mt: 1 }}>Loading...</Box>
-      </StyledGridOverlay>
-    );
-  }
+          <path
+            className="ant-empty-img-3"
+            d="M149.121 33.292l-6.83 2.65a1 1 0 0 1-1.317-1.23l1.937-6.207c-2.589-2.944-4.109-6.534-4.109-10.408C138.802 8.102 148.92 0 161.402 0 173.881 0 184 8.102 184 18.097c0 9.995-10.118 18.097-22.599 18.097-4.528 0-8.744-1.066-12.28-2.902z"
+          />
+          <g className="ant-empty-img-4" transform="translate(149.65 15.383)">
+            <ellipse cx="20.654" cy="3.167" rx="2.849" ry="2.815" />
+            <path d="M5.698 5.63H0L2.898.704zM9.259.704h4.985V5.63H9.259z" />
+          </g>
+        </g>
+      </svg>
+      <Box sx={{ mt: 1 }}>Loading...</Box>
+    </StyledGridOverlay>
+  );
+}
 
 export const Mobile = ({ children }) => {
   const isMobile = useMediaQuery({
-      query: "(max-width:768px)"
+    query: "(max-width:768px)"
   });
   return <>{isMobile && children}</>
 }
-  
+
 export const Pc = ({ children }) => {
-    const isPc = useMediaQuery({
-        query: "(min-width:769px)"
-    });
-    return <>{isPc && children}</>
+  const isPc = useMediaQuery({
+    query: "(min-width:769px)"
+  });
+  return <>{isPc && children}</>
 }
 
-export default function MyDataGrid({children}) {
-    // response.data의 값을 tasks 안에 넣을려고 useState를 설정해줌
-    const [tasks, setTasks] = useState([]);
+export default function MyDataGrid({ children }) {
+  // response.data의 값을 tasks 안에 넣을려고 useState를 설정해줌
+  const [tasks, setTasks] = useState([]);
 
-    const handleRowClick = (params) => {
-        localStorage.setItem("code2", params.row.id);
-        window.location.href = '/dollidodetail';
-      };
-      
-    // load tasks from the backend when the component is rendered
-      useEffect(() => {
-      axios
-            // lost112의 listitem를 받을려고 axios.get(url주소)로 요청함
-          .get('http://127.0.0.1:8000/post/')
-          .then(response => {
-          setTasks(response.data);
-          });
-      }, []);
-    //   console.log(data)
-    return (
-        // display 
-        <div style={{ marginTop: '7%', height: 800, width: '100%', display: 'flex', flexDirection: 'column' }}>
-            <DataGrid
-              components={{
-                NoRowsOverlay: CustomNoRowsOverlay,
-            }}
-                getRowHeight={() => 'auto'} // 자동으로 높이조절을 하는코드
-                rows={tasks}    
-                columns={columns}
-                getRowId={getRowId}
-                onRowClick={handleRowClick}
-            />
-        </div>
-    );
+  const handleRowClick = (params) => {
+    localStorage.setItem("code2", params.row.id);
+    window.location.href = '/dollidodetail';
+  };
+
+  // load tasks from the backend when the component is rendered
+  useEffect(() => {
+    axios
+      // lost112의 listitem를 받을려고 axios.get(url주소)로 요청함
+      .get('http://127.0.0.1:8000/post/')
+      .then(response => {
+        setTasks(response.data);
+      });
+  }, []);
+  //   console.log(data)
+  return (
+    // display 
+    // <Box>
+    <Box style={{ position: 'relative', marginTop: '7%', height: 800, width: '100%', display: 'flex', flexDirection: 'column' }}>
+      <DataGrid
+        components={{
+          NoRowsOverlay: CustomNoRowsOverlay,
+        }}
+        getRowHeight={() => 'auto'} // 자동으로 높이조절을 하는코드
+        rows={tasks}
+        columns={columns}
+        getRowId={getRowId}
+        onRowClick={handleRowClick}
+      />
+      <Box sx={{
+        position: 'fixed',  left: 20, bottom: 35, display: 'flex', flexDirection: 'column' }}>
+      <Fab href='/adddollido' size='medium' color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+    </Box>
+    </Box >
+
+
+  );
 }
