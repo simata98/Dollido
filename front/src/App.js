ce4cad2b157d6727f59ed506516a17baa5944b56
soon9 <81350489+hsk980911@users.noreply.github.com>
2022-12-29 10:50:30 +0900

    템플릿 Login 적용
    Former-commit-id: 00a8d89709d9c93cff0ce299166a7e6197075315 [formerly 525ff2c28f85aab848872f999ba72a2b391bcccf]
    Former-commit-id: e6ceb06bdaf7e8241e1c42935ea3e2a847ee4be0

--- front/src/App.js
+++ front/src/App.js
@@ -1,41 +1,19 @@
-import React from 'react';
-import { Routes, Route} from "react-router-dom";
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+import ScrollToTop from './components/scroll-to-top';
+import { StyledChart } from './components/chart';
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Agreement from './pages/Agreement';
-import MyDataGrid from './pages/Mydatagrid';
-import Header from "./components/Header";
-import Board from "./pages/Board";
-import Dollidolist from "./pages/Dollidolist";
-import Dollidodetail from "./pages/Dollidodetail";
-import AddDollido from "./pages/AddDollido";
-import Test from "./pages/Test";
+// ----------------------------------------------------------------------
 
-function App() {
+export default function App() {
   return (
-    <React.Fragment>
-      <Header />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/agreement" element={<Agreement />} />
-          <Route path="/mydataGrid" element={<MyDataGrid />} />
-          <Route path="/header" element={<Header />} />
-          <Route path="/board" element={<Board />} />
-          <Route path="/dollidolist" element={<Dollidolist />} />
-          <Route path="/dollidodetail" element={<Dollidodetail />} />
-          <Route path="/adddollido" element={<AddDollido />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </div>
-    </React.Fragment>
+    <ThemeProvider>
+      <ScrollToTop />
+      <StyledChart />
+      <Router />
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ front/src/sections/auth/login/LoginForm.js
@@ -0,0 +1,126 @@
+import { useState } from 'react';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+// import axios from "axios";
+// @mui
+import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import Typography from '@mui/material/Typography';
+// components
+import Iconify from '../../../components/iconify';
+
+
+// ----------------------------------------------------------------------
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        DOLLIDO
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+export default function LoginForm() {
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPw, setInputPw] = useState("");
+  // const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const user = {
+      email: inputEmail,
+      password: inputPw,
+    };
+
+    axios
+      .post('http://localhost:8000/accounts/auth/', user)
+      .then((res) => {
+        if (res.data.token.access && res.data.user.is_active) {
+          localStorage.clear();
+          localStorage.setItem("token", res.data.token.access);
+          localStorage.setItem("is_active", res.data.user.is_active);
+          console.log(res.data.token.access)
+          window.location.href = "/";
+          // navigate('/dashboard', { replace: true });
+        }
+      })
+      .catch((err) => {
+        alert("없는 계정이거나, 이메일 인증이 필요합니다.");
+        setInputEmail("");
+        setInputPw("");
+      });
+  };
+
+    // 입력한 이메일값
+    const handleInputEmail = (e) => {
+      setInputEmail(e.target.value);
+    };
+  
+    // 입력한 패스워드값
+    const handleInputPw = (e) => {
+      setInputPw(e.target.value);
+    };
+
+  return (
+    <>
+      <Stack spacing={3}>
+        {/* <TextField name="email" label="Email address" />
+
+        <TextField
+          name="password"
+          label="Password"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        /> */}
+        <TextField
+          value={inputEmail}
+          onChange={handleInputEmail}
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          value={inputPw}
+          onChange={handleInputPw}
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+      </Stack>
+
+      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+        <Checkbox name="remember" label="Remember me" />
+        <Link variant="subtitle2" underline="hover">
+          Forgot password?
+        </Link>
+      </Stack>
+
+      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+        Login
+      </LoadingButton>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </>
+  );
+}
