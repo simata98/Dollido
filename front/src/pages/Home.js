21b68aa6527fbe59ade99c4765cbf9c59029a0ff
choiseeyou <110075391+choiseeyou@users.noreply.github.com>
2023-01-03 13:41:10 +0900

    axios.get(작성자닉네임수정중)
    Former-commit-id: 9b0fc5622013d50a5c3107e8b1f1b89ceaacf2cb [formerly ef46dcbc918163474ae442d9b28ae000193054c5]
    Former-commit-id: 7bcb405255281f709cb6de3e688c8aacd6d85f10

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import cookies from 'react-cookies';
+import axios from 'axios';
 
 const Home = () => {
+    // const token = cookies.load('access');
+    // const [isAuth, setIsAuth] = useState(false);
+    // const [myname, setMyname] = useState([]);
+
+    // useEffect(() => {
+    //     axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem("token");
+    //     axios.defaults.withCredentials = true
+    //     axios
+    //         // lost112의 listitem를 받을려고 axios.get(url주소)로 요청함
+    //         .get('http://127.0.0.1:8000/accounts/auth/')
+    //         .then(response => {
+    //             // delete axios.defaults.headers.common['Authorization'];
+    //             // setMyname(response.data.username);
+    //             console.log(response)
+    //         });
+    // }, []);
+    // useEffect(() => {
+    //     axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem("token");
+    //     axios
+    //       // lost112의 listitem를 받을려고 axios.get(url주소)로 요청함
+    //       .get('http://127.0.0.1:8000/accounts/auth/')
+    //       .then(response => {
+    //         delete axios.defaults.headers.common['Authorization'];
+    //         console.log(response.data)
+    //         setMyname(response.data);
+    //       });
+    //   }, [myname]);
+    // console.log(myname)
     return (
-        <div className="App">
-            메인 화면 입니다.
+        <div>
+            {/* {myname} */}
         </div>
     )
 };
