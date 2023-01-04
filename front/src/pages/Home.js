419ec39351b224b553566e9ff32a3abc7d13a92d
choiseeyou <110075391+choiseeyou@users.noreply.github.com>
2023-01-04 09:40:31 +0900

    writer포함수정본
    Former-commit-id: 580d5e84e681c49184ccfdc35e0daac0e547d97e [formerly b7e29520193f21330074ddadde121b79b0873dda]
    Former-commit-id: ab3fdc7c94da2c9455c4117405a71f628e9df983

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -2,42 +2,28 @@ import React from "react";
 import { useEffect, useState } from "react";
 import cookies from 'react-cookies';
 import axios from 'axios';
+import Pagination from "react-js-pagination";
+// import './Paging.css';
+const Paging = () => {
 
-const Home = () => {
-    // const token = cookies.load('access');
-    // const [isAuth, setIsAuth] = useState(false);
-    // const [myname, setMyname] = useState([]);
+    const [page, setPage] = useState(1);
 
-    // useEffect(() => {
-    //     axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem("token");
-    //     axios.defaults.withCredentials = true
-    //     axios
-    //         // lost112의 listitem를 받을려고 axios.get(url주소)로 요청함
-    //         .get('http://127.0.0.1:8000/accounts/auth/')
-    //         .then(response => {
-    //             // delete axios.defaults.headers.common['Authorization'];
-    //             // setMyname(response.data.username);
-    //             console.log(response)
-    //         });
-    // }, []);
-    // useEffect(() => {
-    //     axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem("token");
-    //     axios
-    //       // lost112의 listitem를 받을려고 axios.get(url주소)로 요청함
-    //       .get('http://127.0.0.1:8000/accounts/auth/')
-    //       .then(response => {
-    //         delete axios.defaults.headers.common['Authorization'];
-    //         console.log(response.data)
-    //         setMyname(response.data);
-    //       });
-    //   }, [myname]);
-    // console.log(myname)
+    const handlePageChange = (page) => {
+        setPage(page);
+        console.log(page)
+    };
+
     return (
-        <div>
-            {/* {myname} */}
-        </div>
-    )
+        <Pagination
+            activePage={page}
+            itemsCountPerPage={10}
+            totalItemsCount={450}
+            pageRangeDisplayed={5}
+            prevPageText={"‹"}
+            nextPageText={"›"}
+            onChange={handlePageChange}
+        />
+    );
 };
 
-export default Home;
-
+export default Paging;
