7f86e574cd1174c42cd233a2f269eace5cc9e5a7
simata98 <wlsrnr905@naver.com>
2023-01-04 17:32:41 +0900

    이거되는듯?
    Former-commit-id: b31bdac3a56b62ec9e20ddbd51208e1e788830ef [formerly b438bc8245797db9693692dab1daec2f8aed4374]
    Former-commit-id: 1abd80f4813dbf0ccfeabc149d099807e91aaedd

--- mainpage/views.py
+++ mainpage/views.py
@@ -8,37 +8,39 @@ from rest_framework import status
 
 @api_view(['GET'])
 def Mainpage_info(request):    
-    
-    today = date.today()
-    today = today.strftime('%Y-%m-%d')
-    
-    # 20일 전 ~ 오늘, 집계된 습득물 개수
-    res = {'dollido_cnt':{},
-           'api_cnt':{},
-           'total_cnt':{}
-           }
-    
-    wallet_cnt = 0
-    phone_cnt = 0
-    for i in range(9,-1,-1):
-        day = date.today() - timedelta(i)
-        day = day.strftime('%Y-%m-%d')
-        temp = Stat_info.objects.get(date=day)
+    try:
+        today = date.today()
+        today = today.strftime('%Y-%m-%d')
         
-        res['dollido_cnt'][day]= temp.dollido_wallet_cnt + temp.dollido_phone_cnt
-        res['api_cnt'][day] = temp.lost112_wallet_cnt + temp.lost112_phone_cnt
-        res['total_cnt'][day] = temp.total_cnt
+        # 20일 전 ~ 오늘, 집계된 습득물 개수
+        res = {'dollido_cnt':{},
+            'api_cnt':{},
+            'total_cnt':{}
+            }
         
-        wallet_cnt = wallet_cnt + temp.dollido_wallet_cnt + temp.lost112_wallet_cnt
-        phone_cnt = phone_cnt + temp.dollido_phone_cnt + temp.lost112_phone_cnt
+        wallet_cnt = 0
+        phone_cnt = 0
+        for i in range(9,-1,-1):
+            day = date.today() - timedelta(i)
+            day = day.strftime('%Y-%m-%d')
+            temp = Stat_info.objects.get(date=day)
+            
+            res['dollido_cnt'][day]= temp.dollido_wallet_cnt + temp.dollido_phone_cnt
+            res['api_cnt'][day] = temp.lost112_wallet_cnt + temp.lost112_phone_cnt
+            res['total_cnt'][day] = temp.total_cnt
+            
+            wallet_cnt = wallet_cnt + temp.dollido_wallet_cnt + temp.lost112_wallet_cnt
+            phone_cnt = phone_cnt + temp.dollido_phone_cnt + temp.lost112_phone_cnt
+            
+        res['wallet_cnt'] = wallet_cnt
+        res['phone_cnt'] = phone_cnt
         
-    res['wallet_cnt'] = wallet_cnt
-    res['phone_cnt'] = phone_cnt
-    
-    data = ApiListId.objects.all().order_by('-fdYmd')[:20]
-    serializer = ApiSerializer(data, many=True)
-    res['current_lost'] = serializer.data
-    return Response(res)
+        data = ApiListId.objects.all().order_by('-fdYmd')[:20]
+        serializer = ApiSerializer(data, many=True)
+        res['current_lost'] = serializer.data
+        return Response(res)
+    except:
+        return Response(res, status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def Mainpage_detail(request, pk):
