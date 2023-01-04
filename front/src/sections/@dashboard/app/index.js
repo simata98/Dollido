2357f5a218815e486d02ee95d1e36d62e0382bc0
choiseeyou <110075391+choiseeyou@users.noreply.github.com>
2023-01-04 16:07:37 +0900

    merged_choi
    Former-commit-id: b31e58ad09f30094b94949d741e3d13e7d36193f [formerly a23d500ef824a25fe80c61ed72dbddc2b44c5f2d]
    Former-commit-id: 63c1343c2f1217ed312fb174c2592223de89c943

---
+++ front/src/sections/@dashboard/app/index.js
@@ -0,0 +1,4 @@
+export { default as AppNewsUpdate } from './AppNewsUpdate';
+export { default as AppCurrentVisits } from './AppCurrentVisits';
+export { default as AppWebsiteVisits } from './AppWebsiteVisits';
+export { default as AppWidgetSummary } from './AppWidgetSummary';
---
+++ front/src/sections/@dashboard/products/ProductCard.js
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types';
+// @mui
+import { Box, Card, Link, Typography, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
+// utils
+import { fCurrency } from '../../../utils/formatNumber';
+// components
+import Label from '../../../components/label';
+import { ColorPreview } from '../../../components/color-utils';
+
+// ----------------------------------------------------------------------
+
+const StyledProductImg = styled('img')({
+  top: 0,
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  position: 'absolute',
+});
+
+// ----------------------------------------------------------------------
+
+ShopProductCard.propTypes = {
+  product: PropTypes.object,
+};
+
+export default function ShopProductCard({ product }) {
+  const { atcId, category, clrNm, depPlace, fdFilePathImg, fdPrdNm, fdSbjt, fdYmd } = product;
+  const link = '/dashboard/lost112/'.concat(atcId)
+  console.log(link)
+  return (
+    <Card id={atcId}>
+      <Box sx={{ pt: '100%', position: 'relative' }}>
+        {/* {status && (
+
+          <Label
+            variant="filled"
+            color={(status === 'sale' && 'error') || 'info'}
+            sx={{
+              zIndex: 9,
+              top: 16,
+              right: 16,
+              position: 'absolute',
+              textTransform: 'uppercase',
+            }}
+          >
+            {status}
+          </Label>
+        )} */}
+        <StyledProductImg alt={category} src={fdFilePathImg} />
+
+      </Box>
+      
+      <Stack spacing={2} sx={{ p: 3 }}>
+        <Link href={link} color="inherit" underline="hover">
+          <Typography variant="h6" noWrap>
+            {category}
+          </Typography>
+        </Link>
+
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          {/* <ColorPreview colors={colors} /> */}
+          <Typography variant="subtitle1">
+            {fdYmd}
+          </Typography>
+          <Typography variant="subtitle1">
+            &nbsp;
+            {clrNm}
+          </Typography>
+        </Stack>
+      </Stack>
+    </Card>
+  );
+}
