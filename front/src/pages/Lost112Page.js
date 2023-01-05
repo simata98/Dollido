6a0d535edaee35e6c0f4ced617233f9bde8e3ed9
soon9 <81350489+hsk980911@users.noreply.github.com>
2023-01-05 17:03:50 +0900

    필터 오류 수정 완료
    Former-commit-id: 7458b08ee0694223feda38f56e6fbe50ef759a59 [formerly f2f810139013451a0a590dd02db055d95806caab]
    Former-commit-id: 811e9ab32a6c85cab509813b079fc37e644a2a67

--- front/src/pages/Lost112Page.js
+++ front/src/pages/Lost112Page.js
@@ -93,22 +93,6 @@ export default function DollidoPage({ products }) {
     setColor(value)
   };
 
-  const handleSearch = () => {
-
-    useEffect(() => {
-      const getData = async () => {
-        const response = await axios.post(
-          'http://127.0.0.1:8000/lost112/', attr
-        );
-        setTasks(response.data);
-        console.log(tasks)
-      };
-      getData();
-    }, []);
-    // setTasks(response.data);
-    console.log(tasks)
-    setOpenFilter(false)
-  }
 
   // 정렬 ---------------------------------------
   const [open, setOpen] = useState(null);
